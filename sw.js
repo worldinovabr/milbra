ea81a15c10dd17366e51447bbacb9bf3064d2042
worldinovabr <worldinovabr@users.noreply.github.com>
2026-07-16 22:45:29 -0300

    Adiciona suporte PWA

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'pcp-milbra-v1';
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.pathname.endsWith('/firebase-config.json')) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put('./index.html', clone));
+                    return response;
+                })
+                .catch(() => caches.match('./index.html'))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(cached => cached || fetch(event.request))
+    );
+});
